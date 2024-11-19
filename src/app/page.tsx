c226c49d3d9b34241dca45777bba76afd0ee47d4
forest-k <bstnqls@naver.com>
2024-11-20 03:18:32 +0900

    QTVY-55 지라 연동 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
+            Jira 연동 테스트
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
